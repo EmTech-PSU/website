4e43efd23e972032a1d4d69f37729255415c25d5
Collin Kleest <collinkleest@gmail.com>
2020-08-15 22:14:05 -0400

    lab section editions

--- src/Components/Labs/index.js
+++ src/Components/Labs/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {
+    LabHeading,
+    LabSelection,
+    LabSelectionCard
+}
+    from './LabElements';
 
 const Labs = () => {
     return (
-        <h1>Labs</h1>
+        <div>
+            <LabHeading />
+            <LabSelection>
+                <LabSelectionCard
+                    labName="NGINX Lab"
+                    src="https://clouddocs.f5.com/training/community/nginx/html/_images/module1.png"
+                    alt="nginx logo"
+                    anchorLink="#"
+                    labDesc="NGINX Lab for learning about web servers and basic linux commands"
+                />
+                <LabSelectionCard
+                    labName="Docker Lab"
+                    src="https://i0.wp.com/www.docker.com/blog/wp-content/uploads/2013/11/homepage-docker-logo.png?resize=300%2C248&ssl=1"
+                    alt="docker logo"
+                    anchorLink="#"
+                    labDesc="Docker is a container solution for building applications"
+                />
+                <LabSelectionCard
+                    labName="Linux/Git Lab"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/150px-Tux.svg.png"
+                    alt="linux logo"
+                    anchorLink="#"
+                    labDesc="Docker is a container solution for building applications"
+                />
+            </LabSelection>
+        </div>
     )
 }
 
-export default Labs; 
+export default Labs;
